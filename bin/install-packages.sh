9009a2a4f1cf80f4f63782b9b6e994a7f909daab
Ryan Williams <ryan.williams@postlight.com>
2018-09-26 11:21:08 -0400

    feat: install some useful plugins

--- bin/install-packages.sh
+++ bin/install-packages.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env sh
 
-# Remove bundled plugins.
-wp plugin uninstall --deactivate --skip-packages \
-	akismet \
-	hello
-
 # This theme is (nearly) empty so nothing displays on the frontend. REST API still works.
 wp theme install --activate https://github.com/modelm/no-theme/archive/master.zip
 
-# Remove bundled themes.
-wp theme delete --skip-packages \
-	twentyfifteen \
-	twentysixteen \
-	twentyseventeen
+wp plugin install --activate \
+	gutenberg \
+	pods \
+	wp-smushit \
+	rest-api-filter-fields \
+	wp-rest-filter \
+	password-protected \
+	fakerpress \
+	userswitcher \
+	query-monitor \
+	debug-bar \
+	debug-bar-actions-and-filters-addon \
+	debug-bar-constants \
+	debug-bar-list-dependencies \
+	debug-bar-post-types \
+	debug-bar-remote-requests \
+	debug-bar-slow-actions \
+	debug-bar-taxonomies \
+	debug-bar-transients
